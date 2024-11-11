bdc04dbe48379e6a15e86fb77dd9f5315ad3c6ce
MagnusJorg <eaa23mojoe@students.eaaa.dk>
2024-11-11 13:11:38 +0100

    fev

--- scripts.js
+++ scripts.js
@@ -4,15 +4,15 @@ const kr3 = document.getElementById("kr3");
 const kr4 = document.getElementById("kr4");
 
 function flytKrager() {
-    const scrollPosition = window.scrollY;
-    const videoSection = document.querySelector('.video').offsetTop;
+    const scrollPosition = Math.round(window.scrollY);
+    const ksektion = document.querySelector('.video').offsetTop;
 
-    if (scrollPosition >= videoSection) {
-        kr1.style.transform = `translateX(${scrollPosition - videoSection}px)`;
-        kr2.style.transform = `translateX(${scrollPosition - videoSection}px)`;
-        kr3.style.transform = `translateX(${scrollPosition - videoSection}px)`;
-        kr4.style.transform = `translateX(${scrollPosition - videoSection}px)`;
+    if (scrollPosition >= ksektion) {
+        kr1.style.transform = `translateX(${scrollPosition - ksektion}px)`;
+        kr2.style.transform = `translateX(${(scrollPosition - ksektion) * 1.2}px)`;
+        kr3.style.transform = `translateX(${-(scrollPosition - ksektion) * 0.8}px)`; // Flyver mod venstre
+        kr4.style.transform = `translateX(${-(scrollPosition - ksektion) * 1.5}px)`; // Flyver mod venstre
     }
 }
 
-window.addEventListener('scroll', flytKrager);
+window.onscroll = flytKrager;
